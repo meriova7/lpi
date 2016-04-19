fe9212a2deb765054e61a2bd6cc0c693749aeca5
meriova7 <mmary9325@gmail.com>
2016-04-19 20:24:10 +0200

    Update builder.py

--- cv07/builder.py
+++ cv07/builder.py
@@ -4,16 +4,49 @@ class TableauBuilder(object):
     def build(self, signedFormulas):
         """ Vytvori a vrati uzavrete alebo uplne tablo pre zoznam oznacenych formul. """
 
-        # vyplnime prve vrcholy podla zoznamu vstupnych formul
-        tabl = tableau.Tableau()
+        alphas = []
+        betas = []
+        branch = {}
+        self.tabl = tableau.Tableau()
         lastNode = None
+        closed = False
         for sf in signedFormulas:
             newNode = tableau.Node(sf)
-            tabl.append(lastNode, newNode)
+            self.tabl.append(lastNode, newNode)
             lastNode = newNode
+            if self.processNode(newNode,alphas,betas,branch):
+                closed = True
+        if not closed:
+            self.expand(lastNode, alphas, betas, branch)
+            # TODO 
+        return self.tabl
 
-        # TODO vytvorit tablo
-
-        return tabl
-
+    def processNode(self, node, alphas, betas, branch):
+        if node.sf.getType() == tableau.ALPHA:
+            alphas.append(node)
+        else:
+            betas.append(node)
+        s = node.sf.f.toString()
+        if s not in branch:
+            branch[s] = [node]
+        else:
+            branch[s].append(node)
+        for otherNode in branch[s]:
+            if otherNode.sf.sign != node.sf.sign:
+                node.close(otherNode)
+                return True
+        return False
+    
+    def expand(self, node, alphas, betas, branch):
+        while alphas != []:
+            newAlphas = []
+            for nd in alphas:
+                for sf in nd.sf.subf():
+                    newNode = tableau.Node(sf,nd)
+                    self.tabl.append(node,newNode)
+                    if self.processNode(newNode, newAlphas, betas, branch):
+                        return
+                    node = newNode
+            alphas = newAlphas
 # vim: set sw=4 ts=8 sts=4 et :
+
